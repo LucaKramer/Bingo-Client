ab50847d616c5e488d7f5df652f05ed9e2d49b33
Luca Kramer <Luca.Kramer@rub.de>
2024-02-08 17:49:38 +0100

    Move BingoBoard in Streamer View

--- src/pages/board.js
+++ src/pages/board.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import backgroundImage from "../Layout-Background.png";
+
 import { useEffect, useState } from "react";
 
 import BingoTable from "../components/BingoTable";
@@ -11,14 +13,32 @@ const MainPage = ({ socket }) => {
         });
     }, [socket]);
 
+    const backgroundStyle = {
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        height: "100vh", // Anpassen Sie die Höhe nach Bedarf
+        // Weitere Stile nach Bedarf hinzufügen
+    };
+
     const [playerInfo] = useState({
         username: "None",
         team: "None",
     });
 
+    const wrapperStyle = {
+        position: "relative",
+        marginLeft: "-258px",   // Move 30px to the right
+        top: "303px",
+        transform: "scale(0.73)",   // Scale down to 60%
+        // Add more styles as needed
+    };
+
     return (
-        <div className="App">
-            <BingoTable socket={socket} playerInfo={playerInfo} />
+        <div style={backgroundStyle}>
+            <div style={wrapperStyle}>
+                <BingoTable socket={socket} playerInfo={playerInfo}/>
+            </div>
         </div>
     );
 };
