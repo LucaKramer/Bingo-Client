5bfab3c5d46101a52bef4615dba6a6bad1b3d578
Luca Kramer <Luca.Kramer@rub.de>
2024-03-07 19:25:39 +0100

    Added checkbox to toggle Balls

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -6,6 +6,8 @@ import BingoTable from "../components/BingoTable";
 import ShiftComponent from "../components/ShiftComponent";
 
 const Admin = ({ socket }) => {
+    const [isCheckboxChecked, setCheckboxChecked] = useState(false);
+
     useEffect(() => {
         socket.on("receive_field", (data) => {
             alert(data.payload);
@@ -18,22 +20,32 @@ const Admin = ({ socket }) => {
     });
 
     const update = () => {
-        socket.emit("refresh_board");
+        socket.emit("refresh_board", {withCheckbox: isCheckboxChecked});
     };
 
     const shuffle = () => {
         socket.emit("shuffle_board");
     };
 
+    const handleCheckboxChange = () => {
+        setCheckboxChecked(!isCheckboxChecked);
+    };
+
     return (
         <div className="App">
+            <input
+                type="checkbox"
+                id="includeCheckbox"
+                checked={isCheckboxChecked}
+                onChange={handleCheckboxChange}
+            />
             <button className="refresh" onClick={() => update()}>
                 Refresh Board
             </button>
             <button className="shuffle" onClick={() => shuffle()}>
                 Shuffle Board
             </button>
-            <ShiftComponent socket={socket} />
+            <ShiftComponent socket={socket}/>
             <div className="row-container">
                 <BingoTable socket={socket} playerInfo={playerInfo}/>
             </div>
